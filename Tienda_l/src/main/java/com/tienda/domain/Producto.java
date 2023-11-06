83ecbaaa8cdb9128261f62409a75f0ab63fa02a3
mayronsb <seguramayron73@gmail.com>
2023-11-05 21:10:21 -0600

    semana 8

--- Tienda_l/src/main/java/com/tienda/domain/Producto.java
+++ Tienda_l/src/main/java/com/tienda/domain/Producto.java
@@ -4,37 +4,38 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import lombok.Data;
 
+
 @Data
 @Entity
 @Table(name="producto")
-
 public class Producto implements Serializable {
-    
-    private static final long serialVersionUID=1l;
-    
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_producto")
-    private Long idProducto;
-    private Long idCategoria;
+    private Long idProducto;    
     private String descripcion;
     private String detalle;
-    public double precio;
-    public int existencias;
+    private double precio;
+    private int existencias;
     private String rutaImagen;
     private boolean activo;
 
-    public Producto() {
+    @ManyToOne
+    @JoinColumn(name="id_categoria")
+    Categoria categoria;
+
+    public Producto() {        
     }
 
-    public Producto(String descripcion, String detalle, double precio, int existencia, String rutaImagen, boolean activo) {
+
+    public Producto(String descripcion, String detalle, double precio, int existencias, String imagen, boolean activo) {
         this.descripcion = descripcion;
         this.detalle = detalle;
         this.precio = precio;
-        this.existencias = existencia;
-        this.rutaImagen = rutaImagen;
+        this.existencias = existencias;
+        this.rutaImagen = imagen;
         this.activo = activo;
     }
-    
-    
 }
